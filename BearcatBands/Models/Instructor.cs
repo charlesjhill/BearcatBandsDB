b3c7d2305d342d5eff7cacdc58e672b0a7f13b66
Charles Hill <charles.hill@etegent.com>
2019-05-02 10:51:30 -0400

    Added an Instructor class

---
+++ BearcatBands/Models/Instructor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BearcatBands.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required, Display(Name = "Last Name"), StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required, Column("FirstName"), Display(Name = "First Name"), StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName => LastName + ", " + FirstMidName;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        public ICollection<EnsembleAssignment> EnsembleAssignments { get; set; }
+    }
+}
---
+++ BearcatBands/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BearcatBands.Models;
+
+namespace BearcatBands.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BearcatBands.Models.BearcatBandsContext _context;
+
+        public IndexModel(BearcatBands.Models.BearcatBandsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Instructor> Instructor { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Instructor = await _context.Instructors.ToListAsync();
+        }
+    }
+}
